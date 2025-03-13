d55de67b1a67a69a16167335c9a3573ee1977c90
luisadelantado-vlc <luis.ad.vlc@gmail.com>
2025-03-13 15:24:30 +0100

    Añadida la configuración del eslint y refactorizado el controller y el service para ajustarlos a las nuevas reglas de eslint

--- src/services/feed.service.ts
+++ src/services/feed.service.ts
@@ -1,96 +1,39 @@
-import { Request, Response } from 'express';
-import Feed from '../models/feed.model';
-import { IFeedDocument } from '../models/feed.model';
+import Feed, { IFeedDocument } from '../models/feed.model';
 import logger from '../utils/logger';
 
+
 export const getAllNews = async (): Promise<IFeedDocument[]> => {
-    try {
-        const news = await Feed.find().lean();
-        logger.info('News retrieved successfully');
-        return news;
-    } catch (err: any) {
-        logger.error('Failed to retrieve news: '+err);
-        throw new Error('Failed to retrieve news: ' + err.message);
-    }
+    return await Feed.find().lean();
 };
 
-export const createNew = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { title, description, url, source, date, category, image, featured } = req.body;
-        logger.info(`Adding a new to the feed with date: ${new Date()}`);
 
-        const newFeed = new Feed({
-            title,
-            description,
-            url,
-            source,
-            date,
-            category,
-            image,
-            featured,
-        });
-    
-        await newFeed.save();
-    
-        res.status(201).json({
-            message: 'New successfully added!',
-            feed: newFeed,
-        });
-    } catch (err: any) {
-        logger.error('Error adding the new: '+err);
-        res.status(500).json({
-            message: 'Error adding the new',
-            error: err.message,
-        });
-    }
-}
- 
-export const getAllNewsByCategory = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { category } = req.params;
-        const news = await Feed.find({ category }).lean();
-        logger.info(`News retrieved successfully by category {category}`);
-        res.json(news);
-    } catch (err: any) {
-        logger.error('Failed to retrieve news '+err);
-        res.status(500).json({ message: 'Failed to retrieve news' });
-    }
-}
+export const createNew = async (newsData: Partial<IFeedDocument>): Promise<IFeedDocument> => {
+    logger.info(`Adding a new feed entry at: ${new Date().toISOString()}`);
+    const newFeed = new Feed(newsData);
+    return await newFeed.save();
+};
 
-export const updateNewById = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { id } = req.params;
-        const { title, description, url, source, date, category, image, featured } = req.body;
-        const updatedNews = await Feed.findByIdAndUpdate(id, { title, description, url, source, date, category, image, featured }, { new: true });
+export const getAllNewsByCategory = async (category: string): Promise<IFeedDocument[]> => {
+    return await Feed.find({ category }).lean();
+};
+
+export const updateNewById = async (id: string, updateData: Partial<IFeedDocument>): Promise<IFeedDocument | null> => {
+    const updatedFeed = await Feed.findByIdAndUpdate(id, updateData, { new: true });
+    if (updatedFeed) {
         logger.info(`News updated successfully with id: ${id}`);
-        res.json(updatedNews);
-    } catch (err: any) {
-        logger.error('Failed to update news '+err);    
-        res.status(500).json({ message: 'Failed to update news' });
     }
-}
+    return updatedFeed;
+};
 
-export const deleteNewById = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { id } = req.params;
-        await Feed.findByIdAndDelete(id);
+export const deleteNewById = async (id: string): Promise<boolean> => {
+    const result = await Feed.findByIdAndDelete(id);
+    if (result) {
         logger.info(`News deleted successfully with id: ${id}`);
-        res.json({ message: 'News deleted successfully' });
-    } catch (err: any) {
-        logger.error('Failed to delete news '+err);
-        res.status(500).json({ message: 'Failed to delete news' });
+        return true;
     }
-}
+    return false;
+};
 
-export const getAllNewsByDate = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { date } = req.params;
-        const news = await Feed.find({ date: { $gte: new Date(date) } }).lean();
-        logger.info(`News retrieved successfully by date ${date}`);
-        res.json(news); 
-    }
-    catch (err: any) {  
-        logger.error('Failed to retrieve news '+err);
-        res.status(500).json({ message: 'Failed to retrieve news' });
-    }
-}
+export const getAllNewsByDate = async (date: string): Promise<IFeedDocument[]> => {
+    return await Feed.find({ date: { $gte: new Date(date) } }).lean();
+};
